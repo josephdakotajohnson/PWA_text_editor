c2eb9d3aa1084848ff64145e5eb036fbda746260
josephdakotajohnson <josephdakotajohnson@gmail.com>
2024-01-16 11:54:08 -0600

    Worked on the webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,13 +17,57 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    //HtmlWebpackPlugin
+    //InjectManifest
+    //WebpackPwaManifest
+    //
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      newWebpackPwaManifest({
+        name: "Text Editor",
+        short_name: "Text Ed",
+        description: "A text editor for editing text.",
+        background_color: "#777777",
+        theme_color: "#777777",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
